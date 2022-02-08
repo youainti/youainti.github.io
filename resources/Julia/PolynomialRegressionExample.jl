6b8098cb1363f35a76d77602aabb66bc85a96ae2
youainti <youainti@protonmail.com>
2022-02-07 16:46:39 -0800

    Fixed some embarrasing errors, added covariance

--- resources/Julia/PolynomialRegressionExample.jl
+++ resources/Julia/PolynomialRegressionExample.jl
@@ -21,9 +21,27 @@ x² = x.^2
 
 X = hcat( intercept, x, x²)
 
-println("Solving the univaritat polynomial equation")
-println(X\y)
+println("Solving the univaritate polynomial equation")
 
+B = X'X\X'y #ordinary least squares estimator
+println(B)
+
+
+#=
+Covariance estimation can use this same approach using the estimator
+
+Q = 1/n * (y-XB) (y-XB)ᵀ
+=#
+
+
+model_residuals = y - X*B
+
+#extract dimensions
+_, p = size(X)
+degrees_freedom = n-p
+
+β_cov = model_residuals'model_residuals/(n-p) * (X'X)^-1
+#basic covariance estimator (doesn't deal with heteroskedasticity or other issues in your estimation)
 
 #=
 One of the benefits of using this algorithm is that it is easily generalizable to multivariate
@@ -31,8 +49,9 @@ polynomials. Consider
 
 y = β₀ + β₁ ̇x+ β₂ x² + γ₁ z + γ₂ z² ̇x+ λ x⋅z
 
-This is simply solved as 
+the βs and covariance of βs are calculated just the same.
 =#
+
 
 #Generate z's
 z = randn(n)
@@ -43,5 +62,21 @@ xz = x .* z
 
 Z = hcat( intercept, x, x², z, z², xz)
 
-println("Solving the multivaritat polynomial equation")
-println(Z\y) #note that this uses the backslash
+println("Solving the multivaritate polynomial equation y = Zγ")
+println(Z'Z\Z'y) #note that this uses the backslash
+
+
+function OLS(Y,A)
+    #=
+    This implements Ordinary Least Squares, with all of the assumptions that entails
+    =#
+    B = A'A\A'Y
+
+    residuals = Y - A*B
+
+    covariance = residuals'residuals * (A'A)^-1
+
+    return (B=B,res=residuals,cov=covariance)
+end
+
+println(OLS(y,Z))
